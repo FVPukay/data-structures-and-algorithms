70057a82f20527dd7adcdc90d83161b474ce5033
frederickpukay <28515879+FVPukay@users.noreply.github.com>
2022-12-15 20:21:32 -0800

    Leetcode: easy: isIsomorphic.js

--- Leetcode/easy/isIsomorphic.js
+++ Leetcode/easy/isIsomorphic.js
@@ -9,40 +9,36 @@
  * @return {boolean}
  */
  var isIsomorphic = function(s, t) {
-    // Time Complexity: O(N)
-    // Space Complexity: O(N)
-    
-    const mapS = {};  // Space: O(N)
-    const mapT = {};  // Space: O(N)
-    
-    // Time: O(N)
-    for (let i = 0; i < s.length; i++) {
-        if (mapS[s[i]] === undefined || mapS[s[i]] === t[i]) {
-            mapS[s[i]] = t[i];
-        } else {
-            return false;
-        }
-    }
-    
-    // Time: O(N)
-    for (let i = 0; i < s.length; i++) {
-        if (mapT[t[i]] === undefined || mapT[t[i]] === s[i]) {
-            mapT[t[i]] = s[i];
-        } else {
-            return false;
-        }
-    }
-    
-    return true;
+     // Time Complexity: O(N)
+     // Space Complexity: O(1)
+     
+     // Explanation why space complexity is constant time
+     // The size of the ASCII character set is fixed (256) and the keys
+     // in the Javascript objects, mapS and MapT, are valid 
+     // ASCII characters
+     
+     const mapS = {};  // Space: O(N)
+     const mapT = {};  // Space: O(N)
+     
+     // Time: O(N)
+     for (let i = 0; i < s.length; i++) {
+         if (mapS[s[i]] === undefined || mapS[s[i]] === t[i]) {
+             mapS[s[i]] = t[i];
+         } else {
+             return false;
+         }
+     }
+     
+     // Time: O(N)
+     for (let i = 0; i < s.length; i++) {
+         if (mapT[t[i]] === undefined || mapT[t[i]] === s[i]) {
+             mapT[t[i]] = s[i];
+         } else {
+             return false;
+         }
+     }
+     
+     return true;
 };
 
 module.exports = isIsomorphic;
-
-/*
-
-Success
-Details 
-Runtime: 90 ms, faster than 82.53% of JavaScript online submissions for Isomorphic Strings.
-Memory Usage: 43.2 MB, less than 62.35% of JavaScript online submissions for Isomorphic Strings.
-
-*/
