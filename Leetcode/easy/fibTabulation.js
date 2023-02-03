527ac214ca30f48137bf90f10cff3ee764fc063c
frederickpukay <28515879+FVPukay@users.noreply.github.com>
2023-02-02 22:01:02 -0800

    Leetcode: easy: fibTabulation.js

---
+++ Leetcode/easy/fibTabulation.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    // Tabulation Solution
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+
+    if (n <= 1) return n;  // constraint: 0 <= n <= 30
+    const table = [0, 1];
+
+    for(let i = 2; i <= n; i++) {
+        table.push(table[i-1] + table[i-2]);
+    }
+    
+    return table[n];
+};
+
+/*
+
+Runtime
+66 ms
+Beats
+73.99%
+Memory
+41.5 MB
+Beats
+87.27%
+
+*/
