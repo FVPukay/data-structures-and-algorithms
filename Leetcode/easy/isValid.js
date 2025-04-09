a36ea65fbc635d82ffdb152fd87582d7897c03c1
FVPukay <28515879+FVPukay@users.noreply.github.com>
2025-04-08 17:43:04 -0700

    Leetcode: easy: isValid.js

---
+++ Leetcode/easy/isValid.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  const stack = [];
+  const brackets = {
+      "(": ")",
+      "{": "}",
+      "[": "]",
+  };
+  for (const b of s) {
+      // if an opening bracket
+      if (b in brackets) {
+          stack.push(b);
+      // else is closing bracket
+      } else {
+          if (!stack.length) {
+              return false;
+          }
+          const prevOpenB = stack.pop();
+          if (brackets[prevOpenB] !== b) {
+              return false;
+          }
+      }
+  }
+  return !stack.length;
+};
